ec8002244b1079293f4ae277184b838874c289e3
Angelique99 <angelique.touraine@gmail.com>
2026-02-13 01:02:54 +0100

    Ajout des boutons filtres complet

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,3 +1,4 @@
+// récupération des travaux + affichage dynamique de la galerie //
 window.addEventListener("DOMContentLoaded", getworks);
 
 async function getworks() {
@@ -20,9 +21,42 @@ function dossierFigures(data) {
     if (!gallery) return console.error(".gallery introuvable");
 
     const figure = document.createElement("figure");
+
+    figure.dataset.category = data.category.name.toLowerCase();
+
     figure.innerHTML = `
         <img src="${data.imageUrl}" alt="${data.title}">
         <figcaption>${data.title}</figcaption>
     `;
     gallery.appendChild(figure);
 }
+
+// Filtres //
+const buttons = document.querySelectorAll("#filtres button");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", function () {
+
+        const category = button.dataset.category;
+        const figures = document.querySelectorAll(".gallery figure");
+
+        figures.forEach(figure => {
+
+            if (category === "tous") {
+                figure.style.display = "block";
+            } 
+            else if (figure.dataset.category === category) {
+                figure.style.display = "block";
+            } 
+            else {
+                figure.style.display = "none";
+            }
+
+        });
+
+    });
+
+});
+
+// Page de connexion //
